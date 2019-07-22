146382e8a49e587004a0c4b28cfe1419f16d3e00
Adityafr <adityapatwa.ap@gmail.com>
2019-07-22 11:09:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/layandrel/Activty_Relatve.kt
@@ -0,0 +1,12 @@
+package com.example.layandrel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activty_Relatve : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activty__relatve)
+    }
+}
